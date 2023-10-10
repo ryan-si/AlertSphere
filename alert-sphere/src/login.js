4a45aeabd74b96280fcdc4ee067400a747b6277c
sizhengping_USTB <sizhengping@hotmail.com>
2023-10-10 18:02:25 +1100

    Update Login and Register Page
    Update Login and Register Page

---
+++ alert-sphere/src/login.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./login.css";
+// import styles from "./signin.module.css";
+
+export default function Signin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
+  const [token, setToken] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, []);
+
+  function login() {
+    const url = "";
+    fetch(url, {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: email, password: password }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.error !== 200) {
+          throw new Error(res.msg);
+        }
+        return res;
+      })
+      .then((res) => {
+        sessionStorage.setItem("token", res.data.token);
+      })
+      .catch((err) => setMsg(err.msg));
+  }
+
+  useEffect(() => {
+    if (token) {
+      navigate("/new-page");
+      sessionStorage.setItem("email", email);
+    }
+  }, [msg, token, email, navigate]);
+
+  return (
+    <div className="flex min-h-screen flex-1 flex-auto justify-center px-6 py-12 lg:px-8 bg-gray-200">
+      <div className="w-3/5 h-min bg-white rounded-lg shadow">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm ">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm mt-5">
+          <form className="space-y-6" onSubmit={login}>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  onChange={(event) => setEmail(event.target.value)}
+                  className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-5 text-center text-sm text-gray-500">
+            Not a member?{" "}
+            <a
+              href="/register"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Register a New Account
+            </a>
+            <div>&nbsp;</div>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ alert-sphere/src/register.js
@@ -0,0 +1,209 @@
+import React, { useState } from "react";
+import "./register.css";
+
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [address1, setAddress1] = useState("");
+  const [address2, setAddress2] = useState("");
+  const [state, setState] = useState("");
+
+  return (
+    <div className="flex min-h-screen flex-1 justify-center px-6 py-12 lg:px-8 bg-gray-200">
+      <div className="h-full bg-white rounded-lg shadow w-3/5">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Register a New Account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="uesrname"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="uesrname"
+                  name="uesrname"
+                  type="uesrname"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="phone"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Mobile Number
+              </label>
+              <div className="mt-2">
+                <input
+                  id="mobile"
+                  name="mobile"
+                  type="mobile"
+                  autoComplete="mobile"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                {/* <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div> */}
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="address1"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Address Line 1
+                </label>
+                {/* <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div> */}
+              </div>
+              <div className="mt-2">
+                <input
+                  id="address1"
+                  name="address1"
+                  type="address1"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="address2"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Address Line 2
+                </label>
+                <div className="text-sm">
+                  <text className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Optional
+                  </text>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="address2"
+                  name="address2"
+                  type="address2"
+                  className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="state"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  State
+                </label>
+                {/* <div className="text-sm">
+                  <text className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    
+                  </text>
+                </div> */}
+              </div>
+              <div className="mt-2">
+                {/* <input
+                  id="address2"
+                  name="address2"
+                  type="address2"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                /> */}
+                <select className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                  <option className="bg-gray-200 hover:bg-gray-300" value="1">
+                    NSW
+                  </option>
+                  <option className="bg-gray-200 hover:bg-gray-300" value="2">
+                    Option 2
+                  </option>
+                  <option className="bg-gray-200 hover:bg-gray-300" value="3">
+                    Option 3
+                  </option>
+                </select>
+              </div>
+            </div>
+
+            <div className="mb-6">
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500"
+              >
+                Sign up
+              </button>
+            </div>
+            <div>&nbsp;</div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
