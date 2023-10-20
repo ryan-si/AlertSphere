251e29c50b3b354583f48344b04f1406a9d4a11b
li1085 <lfd1085@gmail.com>
2023-10-20 17:22:59 +1100

    apiAdjust

--- alert-sphere/src/App.js
+++ alert-sphere/src/App.js
@@ -12,25 +12,28 @@ import Favorites from "./Favorites";
 import NewsPage from "./NewsPage";
 import Settings from "./Settings";
 import SettingDetail from "./SettingDetail";
+import { LoadScript } from '@react-google-maps/api';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/news" element={<NewsPage />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/settingDetail" element={<SettingDetail />} />
+    <LoadScript googleMapsApiKey="AIzaSyCxBkbx6cQ88YShFQTJLXBlhTu3mcupSv0">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/settingDetail" element={<SettingDetail />} />
 
-        <Route path="/homePageTest" element={<HomePageTest />} />
-        <Route path="/warnings" element={<Warnings />} />
-        <Route path="/healthTips" element={<HealthTips />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-      <div className="App"></div>
-    </BrowserRouter>
+          <Route path="/homePageTest" element={<HomePageTest />} />
+          <Route path="/warnings" element={<Warnings />} />
+          <Route path="/healthTips" element={<HealthTips />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+        <div className="App"></div>
+      </BrowserRouter>
+    </LoadScript>
   );
 }
 
--- alert-sphere/src/components/MapComponent.js
+++ alert-sphere/src/components/MapComponent.js
@@ -1,15 +1,13 @@
 import React from "react";
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap } from '@react-google-maps/api';
 function MapComponent() {
   return (
-    <LoadScript googleMapsApiKey="AIzaSyCxBkbx6cQ88YShFQTJLXBlhTu3mcupSv0">
       <GoogleMap
         mapContainerStyle={{ width: '100%', height: '100%' }}
         center={{ lat: -3.745, lng: -38.523 }}
         zoom={10}
       >
       </GoogleMap>
-    </LoadScript>
   );
 }
   
